786d989e452b53b861b669ccf0e7fc3d1464a3ff
Abdullah-max-eng <abdullahfazli8.af@gmail.com>
2023-01-29 15:51:43 +0300

    Project Upload

---
+++ CardView- Mobile App/CardView/app/src/main/java/com/example/cardview/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.cardview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void action(View view) {
+        Toast.makeText(getApplicationContext(),"Abdullah",Toast.LENGTH_SHORT).show();
+    }
+}
